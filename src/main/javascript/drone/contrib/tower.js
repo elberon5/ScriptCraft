b5179ec0f78015e3072c125c3502862d81ede9e4
Alex Rystrom <elberon5@gmail.com>
2013-02-26 23:08:59 -0500

    move to correct location, fixed ladder, added windows

--- src/main/javascript/drone/contrib/tower.js
+++ src/main/javascript/drone/contrib/tower.js
@@ -3,7 +3,9 @@ load(__folder + "drone.js");
 // constructs a tower with steps
 //
 Drone.extend('tower', function(floors, side) {
-  var floorHeight = 4;
+  var floorHeight = 4, brick = 98, window = 102;
+  var ladder = '65:' + Drone.PLAYER_SIGN_FACING[(this.dir)];
+  var torch = '50:' + Drone.PLAYER_TORCH_FACING[this.dir];
 
   if (typeof side == "undefined")
     side = 6;
@@ -13,9 +15,10 @@ Drone.extend('tower', function(floors, side) {
   if (floors < 1 || side < 6)
     throw new java.lang.RuntimeException("Tower must be at least 6 wide and have 1 floor");
 
-  var brick = 98, window = 102;
-  var ladder = '65:' + Drone.PLAYER_SIGN_FACING[(this.dir)];
-  var torch = '50:' + Drone.PLAYER_TORCH_FACING[this.dir];
+  // make sure side is even;
+  if (side%2)
+    side++;
+  var halfway = side/2;
 
   // remove what's there
   this.box(0, side, 40, side);
@@ -32,8 +35,12 @@ Drone.extend('tower', function(floors, side) {
   this.move('tower').up(1).right(1).door() // double doors
     .back().left().up().box(torch) // left torch
     .right(2).box(torch); // right torch
-  // window
-
+  // windows
+  this.move('tower').up(2).fwd(halfway-1).box(window)
+    .fwd(halfway).right(halfway-1).box(window)
+    .right(halfway).back(halfway-1).box(window)
+    .back(halfway).left(halfway-1).box(window);
+  
   for(x = 1;x < floors; x++)  {
     // floor is the previous ceiling, make a hole for the steps
     this.move('tower').up(5 * x).fwd(side-2).right(1)
@@ -46,9 +53,14 @@ Drone.extend('tower', function(floors, side) {
       .box(brick, side, 1, side);
     // add a ladder
     this.move('tower').right(1).up(5 * (x-1)).up(1).fwd(side - 2) // move inside tower
-      .box(ladder, 1,(floors * floorHeight)-3,1);
+      .box(ladder, 1,(floorHeight+ 1),1);
     // add a torch on each floor
     this.move('tower').up(5 * x).fwd(side-2).right(2).up(3)
       .box(torch);
+  // windows
+    this.move('tower').up(5 * x).up(2).fwd(halfway-1).box(window)
+      .fwd(halfway).right(halfway-1).box(window)
+      .right(halfway).back(halfway-1).box(window)
+      .back(halfway).left(halfway-1).box(window);
   }
 });
